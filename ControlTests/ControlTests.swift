939018c9ed5b54437a62670bee328404abadfc40
zhufucdev <zhufuzhufu1@gmail.com>
2026-01-16 07:46:25 +0800

    Initial Commit

---
+++ ControlTests/ControlTests.swift
@@ -0,0 +1,16 @@
+//
+//  ControlTests.swift
+//  ControlTests
+//
+//  Created by Steve Reed on 2026/1/16.
+//
+
+import Testing
+
+struct ControlTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
