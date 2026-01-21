fd2ca2eed28b6e2fbe865ecfe2b5d79c54061e3f
zhufucdev <zhufuzhufu1@gmail.com>
2026-01-21 08:24:08 +0800

    [fix](api client) delete requests not working

--- Control/APIClientHelper.swift
+++ Control/APIClientHelper.swift
@@ -4,8 +4,46 @@ import OpenAPIClient
 extension OpenAPIClientAPIConfiguration {
     func alternate(basePath: String, postAuthKey: String) {
         self.basePath = basePath
-        self.customHeaders = [
-            "X-Post-Auth-Key": postAuthKey
+        customHeaders = [
+            "X-Post-Auth-Key": postAuthKey,
         ]
+        interceptor = ControlOpenAPIInterceptor(baseURL: URL(string: basePath)!)
+    }
+}
+
+struct ControlOpenAPIInterceptor: OpenAPIInterceptor {
+    private let origin: String?
+    
+    init(baseURL: URL) {
+        var components = URLComponents(url: baseURL, resolvingAgainstBaseURL: false)!
+        components.path = ""
+        self.origin = components.url?.absoluteString
+    }
+    
+    func intercept<T>(urlRequest: URLRequest, urlSession: URLSessionProtocol, requestBuilder: RequestBuilder<T>, completion: @escaping (Result<URLRequest, Error>) -> Void) {
+        if let origin {
+            switch urlRequest.httpMethod {
+            case "PUT":
+                fallthrough
+            case "POST":
+                fallthrough
+            case "OPTIONS":
+                fallthrough
+            case "PATCH":
+                fallthrough
+            case "DELETE":
+                var newRequest = urlRequest
+                newRequest.addValue(origin, forHTTPHeaderField: "Origin")
+                completion(.success(newRequest))
+                return
+            default: break
+            }
+        }
+        
+        completion(.success(urlRequest))
+    }
+
+    func retry<T>(urlRequest: URLRequest, urlSession: URLSessionProtocol, requestBuilder: RequestBuilder<T>, data: Data?, response: URLResponse?, error: Error, completion: @escaping (OpenAPIInterceptorRetry) -> Void) {
+        completion(.dontRetry)
     }
 }
