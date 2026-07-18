3b5bedfd2cb487bb9eb93413c849a7f35972e5e2
zhufucdev <zhufuzhufu1@gmail.com>
2026-07-18 09:15:28 +0800

    update minimum watchos version of openapi client

--- ApiClient/Package.swift
+++ ApiClient/Package.swift
@@ -8,7 +8,7 @@ let package = Package(
         .iOS(.v12),
         .macOS(.v10_13),
         .tvOS(.v12),
-        .watchOS(.v4),
+        .watchOS(.v9),
     ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
